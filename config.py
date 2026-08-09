27ba411f827c68bb2f94a040e034a60349d329f4
xddfgfhf <148470829+szsffxg@users.noreply.github.com>
2026-08-10 00:45:16 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,26 @@
 import os
-import pytz
-from aiogram import Bot, Dispatcher
-from aiogram.fsm.storage.memory import MemoryStorage
+from dotenv import load_dotenv
 
-TOKEN = os.getenv('TOKEN')
-OWNER_ID = int(os.getenv('ADMIN', 0))
-CHANNEL_ID = int(os.getenv('PRIVATE_CHANNEL_ID', 0))
-CHAT_ID = int(os.getenv('PRIVATE_CHAT_ID', 0))
+load_dotenv()
 
-DATABASE_URL = 'sqlite+aiosqlite:///gift_bot.db'
+# --- ОСНОВНЫЕ НАСТРОЙКИ ---
+TOKEN = os.getenv("TOKEN")
+ADMIN = int(os.getenv("ADMIN", 0))
 
-MSK = pytz.timezone('Europe/Moscow')
-bot = Bot(token=TOKEN)
-dp = Dispatcher(storage=MemoryStorage())
+PRIVATE_CHANNEL_ID = os.getenv("PRIVATE_CHANNEL_ID")
+PRIVATE_CHAT_ID = os.getenv("PRIVATE_CHAT_ID")
+
+# --- ПОДКЛЮЧЕНИЕ К БД (POSTGRESQL) ---
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+DB_HOST = os.getenv("DB_HOST")  # Добавьте эту переменную в Railway
+
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+
+# --- ЦЕНЫ ТАРИФОВ (Telegram Stars) ---
+PRICES = {
+    "month": 1,
+    "sixmonth": 500,
+    "year": 900
+}
