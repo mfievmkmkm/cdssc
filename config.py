a7b4dfc4b04802297ff81867958d930217b2526c
mfievmkmkm <kiryushekuc29@gmail.ru>
2026-08-10 00:23:42 +0500

    fix: SQLite вместо PostgreSQL

--- config.py
+++ config.py
@@ -8,13 +8,11 @@ OWNER_ID = int(os.getenv('ADMIN', 0))
 CHANNEL_ID = int(os.getenv('PRIVATE_CHANNEL_ID', 0))
 CHAT_ID = int(os.getenv('PRIVATE_CHAT_ID', 0))
 
-DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_NAME = os.getenv('DB_NAME')
-
-DATABASE_URL = f'postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@postgres_db:5432/{DB_NAME}'
+# SQLite вместо PostgreSQL
+DATABASE_URL = 'sqlite:///gift_bot.db'
 
 MSK = pytz.timezone('Europe/Moscow')
 print(f"TOKEN = {TOKEN}")
+print(f"DATABASE_URL = {DATABASE_URL}")
 bot = Bot(token=TOKEN)
 dp = Dispatcher(storage=MemoryStorage())
